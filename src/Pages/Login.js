d640d7f45e6e3be27722623b6ce11a58d9e87cbd
shubham <shubham@shubhams-MacBook-Air.local>
2024-08-20 23:32:40 +0530

    fire base

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import {  Image, ScrollView, View } from 'react-native';
+import { Image, ScrollView, View } from 'react-native';
 import { _HEADER_TYPE } from '../Util/GlobalConstant';
 import Styles from '../Styles/Styles';
 import { _GLOBAL_COLORS } from '../Styles/StylesConstants';
@@ -8,16 +8,33 @@ import { _FORMS } from '../component/Form/FormConfig';
 import { GlobalContext } from '../../App';
 import { ShowSuccessAlert } from '../Util/GlobalFunction';
 import { localized } from '../component/CommonUtil/CommonUtil';
+import auth from '@react-native-firebase/auth';
+
 
 function Login(props) {
     const { setLoginStatus } = useContext(GlobalContext)
 
     const Dologin = (data) => {
-        ShowSuccessAlert(
-            flag => {setLoginStatus(false)},
-            localized('success_alert_lbl'),
-            localized('login_Success_lbl')
-        )
+        auth()
+            .createUserWithEmailAndPassword('jane.doe@example.com', 'SuperSecretPassword!')
+            .then(() => {
+                console.log('User account created & signed in!');
+                ShowSuccessAlert(
+                    flag => { setLoginStatus(false) },
+                    localized('success_alert_lbl'),
+                    localized('login_Success_lbl')
+                )
+            })
+            .catch(error => {
+                if (error.code === 'auth/email-already-in-use') {
+                    console.log('That email address is already in use!');
+                }
+
+                if (error.code === 'auth/invalid-email') {
+                    console.log('That email address is invalid!');
+                }
+                console.error(error);
+            });
     }
     return (
         <View style={{ flex: 1, backgroundColor: _GLOBAL_COLORS.WHITE }}>
